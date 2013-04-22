ba2cb4332c8470129e5287a08753bb958e46ed37
Daniel Moore <yahivin@gmail.com>
2013-04-22 09:34:32 -0700

    Updating sprockets integration

--- lib/shank.rb
+++ lib/shank.rb
@@ -1,7 +1,14 @@
 require 'sprockets'
 
 module Shank
-  root_dir = File.expand_path(File.dirname(File.dirname(__FILE__)))
+  if defined? ::Rails
+    class Engine < ::Rails::Engine
+      config.paths['app/assets'] = "source"
+    end
+  else
+    root_dir = File.expand_path(File.dirname(File.dirname(__FILE__)))
+    asset_dir = File.join(root_dir, "source")
 
-  ::Sprockets.append_path File.join(root_dir, "source")
+    ::Sprockets.append_path asset_dir
+  end
 end
